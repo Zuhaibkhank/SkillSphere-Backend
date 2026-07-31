18d5fdaae6fc3c963e6104aaa72ee6852e162751
Mohd Zuhaib Khan <zuhaibkhan1915691@gmail.com>
2026-08-01 03:14:18 +0530

    Add HomeController

--- src/main/java/com/skillsphere/backend/controller/HomeController.java
+++ src/main/java/com/skillsphere/backend/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "SkillSphere Backend is Running 🚀";
     }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is Healthy ✅";
+    }
 }
